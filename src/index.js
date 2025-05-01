ebf197c665c86ebf744bbb725d285b976a93d893
Chance <cloricco13@icloud.com>
2025-05-01 18:12:20 -0500

    small changes

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ import { TwitchChat } from "../public/twitch_chat";
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-let chat = new TwitchChat;
+//let chat = new TwitchChat;
 
 export default {
 	async fetch(request, env, ctx) {
+		
 		const url = new URL(request.url);
 		switch (url.pathname) {
 			case '/refresh':
@@ -32,8 +33,8 @@ export default {
 			case '/message':
 				return new Response('Test!');
 			case '/callback':
-				let test = chat.notification(request);
-				console.log(test);
+				//let test = chat.notification(request);
+				//console.log(test);
 				return new Response('callback');
 			default:
 				return new Response('Not Found', { status: 404 });
